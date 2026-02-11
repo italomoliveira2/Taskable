2b2384d05eafb724516993f4b1a2f0bab74674ab
italomoliveira2 <workspace.capturame@gmail.com>
2026-02-11 10:57:05 -0300

    Feat: Tela do login

--- resources/js/pages/Auth/Login.tsx
+++ resources/js/pages/Auth/Login.tsx
@@ -1,22 +1,48 @@
-// import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
+import { Link } from "@inertiajs/react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Login() {
     return (
-        <main className="flex items-center justify-center bg-slate-900 min-h-screen text-gray-100">
-            <Card className="w-full max-w-md bg-slate-800">
-                <CardHeader>
-                    <CardTitle></CardTitle>
-                    <CardDescription>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                </CardContent>
-                <CardFooter className="flex-col gap-2">
-                </CardFooter>
-            </Card>
+        <main className="flex items-center justify-between *:min-h-screen">
+            <div className="bg-blue-200 w-[50%] flex justify-center items-center">
+                banner
+            </div>
+            <div className="w-[50%] flex justify-center items-center">
+                <form className="w-[50%] space-y-4">
+                    <h1 className="text-3xl font-bold text-center">LOGIN</h1>
+                    <FieldGroup className="gap-4">
+                        <Field>
+                            <FieldLabel>Email:</FieldLabel>
+                            <Input />
+                            <FieldError/>
+                        </Field>
+                        <Field>
+                            <FieldLabel>Senha:</FieldLabel>
+                            <Input />
+                            <FieldError></FieldError>
+                        </Field>
+                        <Field>
+                            <div className="flex items-center justify-between text-sm">
+
+                                <div className="flex items-center space-x-2">
+                                    <Checkbox></Checkbox>
+                                    <Label>Lembre de mim</Label>
+                                </div>
+                                <div>
+                                    <Link>Esqueceu a senha?</Link>
+                                </div>
+                            </div>
+                        </Field>
+                        <Field>
+                            <Button>Login</Button>
+                        </Field>
+                    </FieldGroup>
+                </form>
+            </div>
         </main>
     )
 }
